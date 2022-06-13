708085281175e0fa41155aa8a1b1b88bb684f75b
Joshua Stresing <pyrotorch69@yahoo.com>
2022-06-13 03:35:12 -0700

    spotlight work

--- index.js
+++ index.js
@@ -1,5 +1,80 @@
-class Stack {}
+class Stack {
+  #list = [];
+  constructor(initialList) {
+    if (initialList) {
+      this.#list = initialList;
+    }
+  }
+  // adds to end
+  push(item) {
+    this.#list.push(item);
+  }
+  // removes last
+  pop() {
+    return this.#list.pop();
+  }
+  // checks listed index
+  peek() {
+    return this.#list[this.#list.length - 1];
+  }
 
-class Queue {}
+  get readableList() {
+    return this.#list.toString();
+  }
+}
+
+class Queue {
+  #list = [];
+  constructor(initialList) {
+    if (initialList) {
+      this.#list = initialList;
+    }
+  }
+
+  get readableList() {
+    return this.#list.toString();
+  }
+
+  onqueue(item) {
+    this.#list.push(item);
+  }
+
+  dequeue() {
+    return this.#list.shift();
+  }
+
+  get hasNext() {
+    return this.#list.length > 0;
+
+    // this also works because of truthy and falsey statements in javascript, an empty array by default is false for example
+
+    // return !!this.#list.length; ---> the !!makes it into an actual boolean
+  }
+}
+
+function reverse(array) {
+  const stack = new Stack(array);
+  let reversedArray = [];
+  // i is index starting with first, as long as its less then the array length itll keep going through, each time it does 1 it adds 1 to index number
+  for (let i = 0; i < stack.length; i++) {
+    reversedArray = [...reversedArray, stack.pop()];
+  }
+}
+
+// myArray.map((val, index, array) => array[array.length - 1 - index]); -->should work, dunno how to refactor for what she wants.
+
+//   const stack = new Stack(array);
+//   const reversedArray = [];
+//   reversedArray = stack.map(
+//     (val, index, array) => array[array.length - 1 - index]
+//   );
+//   return reversedArray;
+// ?
 
 module.exports = { Stack, Queue };
+
+// partner 1	Stack
+// partner 2	Queue
+// pair	O-1-dequeue
+// demo	reverse-array-with-stack
+// pair	check-syntax
--- index.test.js
+++ index.test.js
@@ -1,9 +1,79 @@
-const { Stack } = require("./index.js");
+const { Stack, Queue } = require("./index.js");
 describe("Stack Class", () => {
-  it("#push should add a new item to the stack ", () => {
+  it("should initialize with an empty list", () => {
     const stack = new Stack();
-    stack.push(1);
-    expect(stack.peek()).toEqual(1);
+    expect(stack.readableList).toEqual("");
   });
-  // add more tests here...
+
+  it("should initialize with an existing list", () => {
+    const stack = new Stack([1, 2, 3]);
+    expect(stack.readableList).toEqual("1,2,3");
+  });
+
+  it("#push should add a new item to the stack", () => {
+    const stack = new Stack([1]);
+    stack.push(2);
+    expect(stack.readableList).toEqual("1,2");
+  });
+
+  it("#pop should remove last item in list", () => {
+    const stack = new Stack([1]);
+    stack.push(2);
+    expect(stack.pop()).toEqual(2);
+    expect(stack.readableList).toEqual("1");
+  });
+
+  it("#peek should return the top of the list", () => {
+    const stack = new Stack([1]);
+    stack.push(2);
+    expect(stack.peek()).toEqual(2);
+    expect(stack.readableList).toEqual("1,2");
+  });
 });
+// start mentality on test=>forwards thinking, test should be outline of how the thing your testing works so you can test it as you make it
+describe("Queue Class", () => {
+  it("should initialize with an empty list", () => {
+    const queue = new Queue();
+    expect(queue.readableList).toEqual("");
+  });
+
+  it("should initialize with an existing list", () => {
+    const queue = new Queue([1, 2, 3]);
+    expect(queue.readableList).toEqual("1,2,3");
+  });
+
+  it("#onqueue should add item to end of que", () => {
+    const queue = new Queue([1, 2, 3]);
+    queue.onqueue(4);
+    expect(queue.readableList).toEqual("1,2,3,4");
+  });
+
+  it("#dequeue should remove item from front of que", () => {
+    const queue = new Queue([1, 2, 3]);
+    // when testing things like array methods, make sure your feeding in the right data type, for example a number vs a string
+    expect(queue.dequeue()).toEqual(1);
+    expect(queue.readableList).toEqual("2,3");
+  });
+
+  it("hasNext should return if there is something in the array", () => {
+    const queue = new Queue([1, 2, 3]);
+    expect(queue.hasNext).toEqual(true);
+  });
+
+  it("hasNext should return if there is something in the array", () => {
+    const queue = new Queue();
+    expect(queue.hasNext).toEqual(false);
+  });
+});
+
+describe("#reverse", () => {
+  it("should reserve an array", () => {
+    expect(reverse([1, 2, 3])).toEqual([3, 2, 1]);
+  });
+});
+// it("#push should add a new item to the stack ", () => {
+//   const stack = new Stack();
+//   stack.push(1);
+//   expect(stack.peek()).toEqual(1);
+// });
+// add more tests here...
